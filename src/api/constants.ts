3eb2913f9897539241e91cffbc031570cf7c35ec
leulseged3@gmail.com <leulseged3@gmail.com>
2022-01-06 20:28:56 +0300

    replace local baseuri with deployed one

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const API_STORE = axios.create({
-  // baseURL: 'https://leulseged-store-api.herokuapp.com/',
-  baseURL: 'http://localhost:3000/'
+  baseURL: 'https://leulseged-store-api.herokuapp.com/',
+  // baseURL: 'http://localhost:3000/'
 });
