08448ddd4e60e9c20b0f0f6f928c60b13266376c
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-06 13:10:52 +0300

    create header and login components

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+import { PageHeader } from 'antd';
+
+export default () => (
+  <PageHeader
+    className="site-page-header"
+    // onBack={() => null}
+    title="Title"
+    subTitle="This is a subtitle"
+  />
+)
---
+++ src/components/Login.tsx
@@ -0,0 +1,83 @@
+import { Form, Input, Button, Checkbox } from 'antd';
+import { useState } from 'react';
+import { login } from '../api/login';
+import { useActions, useProps } from '../hooks';
+
+export default () => {
+  const actions = useActions();
+  const { Auth } = useProps(state => state);
+  const [loginError, setLoginError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onFinish = (values: { email: string, password: string }) => {
+    setLoading(true)
+    login(values.email, values.password)
+      .then(res => actions.Auth.setAuth({ ...res, token: res.access_token }))
+      .catch(() => setLoginError(true))
+      .finally(() => setLoading(false))
+  };
+  console.log(Auth)
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 4,
+      }}
+      // // wrapperCol={{
+      //   span: 16,
+      // }}
+      // initialValues={{
+      //   remember: true,
+      // }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        // label="Email"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your username!',
+          },
+        ]}
+      >
+        <Input placeholder='Enter Email Address' />
+      </Form.Item>
+
+      <Form.Item
+        // label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: 'Please input your password!',
+          },
+        ]}
+      >
+        <Input.Password placeholder='********' />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          // offset: 4,
+          // span: 16,
+        }}
+      >
+        <Button
+          type="primary"
+          htmlType="submit"
+          block
+          loading={loading}
+        >
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
