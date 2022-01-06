0baaa63c69102998108d632f729af36857138f13
Leulseged tilahun <leulseged@eaglelionsystems.com>
2022-01-06 14:45:22 +0300

    display login error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -22,62 +22,68 @@ export default () => {
   };
 
   return (
-    <Form
-      name="basic"
-      labelCol={{
-        span: 4,
-      }}
-      // // wrapperCol={{
-      //   span: 16,
-      // }}
-      // initialValues={{
-      //   remember: true,
-      // }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        // label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your username!',
-          },
-        ]}
-      >
-        <Input placeholder='Enter Email Address' />
-      </Form.Item>
-
-      <Form.Item
-        // label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password placeholder='********' />
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          // offset: 4,
-          // span: 16,
+    <>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 4,
         }}
+        // // wrapperCol={{
+        //   span: 16,
+        // }}
+        // initialValues={{
+        //   remember: true,
+        // }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button
-          type="primary"
-          htmlType="submit"
-          block
-          loading={loading}
+        <Form.Item
+          // label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
         >
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Input placeholder='Enter Email Address' />
+        </Form.Item>
+
+        <Form.Item
+          // label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password placeholder='********' />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            // offset: 4,
+            // span: 16,
+          }}
+        >
+          <Button
+            type="primary"
+            htmlType="submit"
+            block
+            loading={loading}
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+      {
+        loginError &&
+        <h3 style={{ color: 'red' }}>Something went wrong.please try again</h3>
+      }
+    </>
   );
 };
