fddb723323325c1ab041e2d9c8c7c1b6d82a60c7
leulseged3@gmail.com <leulseged3@gmail.com>
2022-01-06 20:22:42 +0300

    toggle api bug fix

--- src/api/toggle-approve.ts
+++ src/api/toggle-approve.ts
@@ -4,7 +4,7 @@ import { getResponseData } from './get-data';
 export const toggleApprove = (id: string, token: string) => {
   API_STORE.defaults.headers.common = { 'Authorization': `Bearer ${token}` }
 
-  return API_STORE.put<IRecord>(`sales/${id}`, {
+  return API_STORE.put<IRecord>(`sales/${id}`,{}, {
     headers: {
       usertype: 'ADMIN',
     }
